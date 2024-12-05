0cd50db094aeb361a8b0b6c661b5528917b7b697
monkjiro <thenullmastermind@gmail.com>
2024-12-05 20:33:35 +0700

    refactor: Optimize text display widget for LLM nodes
    - Simplify text widget creation and management
    - Reduce widget manipulation complexity
    - Improve performance by minimizing widget operations
    - Ensure single text widget is maintained for display

--- src/extensions/core/llm/showText.ts
+++ src/extensions/core/llm/showText.ts
@@ -6,65 +6,34 @@ app.registerExtension({
   name: 'llm.ShowText',
   async beforeRegisterNodeDef(nodeType: any, nodeData: NodeData, app: any) {
     if (nodeData.output?.includes('OUTPUT_TEXT')) {
-      function populateTextWidgets(this: any, textContent: string | string[]) {
-        if (this.widgets) {
-          for (let i = this.widgets.length - 1; i >= 0; i--) {
-            this.widgets[i].onRemove?.()
-          }
-          this.widgets.length = 0
-        }
+      const onExecuted: any = nodeType.prototype.onExecuted
 
-        // Convert input to array and remove empty first element if exists
-        const textLines = [...textContent]
-        if (!textLines[0]) {
-          textLines.shift()
-        }
-
-        // Create text widgets for each line of content
-        for (const textLine of textLines) {
-          const textWidget = ComfyWidgets['STRING'](
-            this,
-            'text',
-            ['STRING', { multiline: true }],
-            app
-          ).widget as any
-          textWidget.inputEl.readOnly = true
-          textWidget.value = textLine
-        }
-
-        // Update node size on next frame to accommodate new widgets
-        requestAnimationFrame(() => {
-          const computedSize = this.computeSize()
-          const newSize = [
-            Math.max(computedSize[0], this.size[0]),
-            Math.max(computedSize[1], this.size[1])
-          ]
-          this.onResize?.(newSize)
-          app.graph.setDirtyCanvas(true, false)
-        })
-      }
-
-      for (let i = nodeData.output.length - 1; i >= 0; i--) {
-        if (nodeData.output[i] === 'OUTPUT_TEXT') {
-          nodeData.output.splice(i, 1)
-          nodeData.output_name?.splice(i, 1)
-          nodeData.output_is_list?.splice(i, 1)
-        }
-      }
-
-      // When the node is executed we will be sent the input text, display this in the widget
-      const onExecuted = nodeType.prototype.onExecuted
       nodeType.prototype.onExecuted = function (message: any) {
         onExecuted?.apply(this, arguments)
-        populateTextWidgets.call(this, message.text)
-      }
 
-      const onConfigure = nodeType.prototype.onConfigure
-      nodeType.prototype.onConfigure = function () {
-        onConfigure?.apply(this, arguments)
-        if (this.widgets_values?.length) {
-          populateTextWidgets.call(this, this.widgets_values)
+        if (this.widgets) {
+          for (let i = 1; i < this.widgets.length; i++) {
+            this.widgets[i].onRemove?.()
+          }
+          this.widgets.length = 1
         }
+
+        // Check if the "text" widget already exists.
+        let textWidget: any =
+          this.widgets &&
+          this.widgets.find((w: any) => w.name === 'displaytext')
+        if (!textWidget) {
+          textWidget = ComfyWidgets['STRING'](
+            this,
+            'displaytext',
+            ['STRING', { multiline: true }],
+            app
+          ).widget
+          textWidget.inputEl.readOnly = true
+          textWidget.inputEl.style.border = 'none'
+          textWidget.inputEl.style.backgroundColor = 'transparent'
+        }
+        textWidget.value = message['text'].join('')
       }
     }
   }
